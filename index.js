facb3950658e0919d6dbd35d027d2539494978aa
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2018-05-14 22:17:56 -0300

    working fetchs

--- index.js
+++ index.js
@@ -5,19 +5,27 @@ const body_parser = require('body-parser')
 const app     = express()
 
 app.use(body_parser.urlencoded({extended:true}))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var fs      = require('file-system')
 var _ 			= require('lodash')
 
-
-app.get('/items/:id', async (req, res) => {
+app.get('/items', async (req, res,next) => {
+  const { search } = req.query
+  const data = await fetch(`https://api.mercadolibre.com/sites/MLA/search?q=:${search}`);
+  const itemsearch = await data.json();
+  res.json({itemsearch})
+})
+app.get('/items/:id', async (req, res,next) => {
   const { id } = req.params
-  const data = await fetch("https://api.mercadolibre.com/");
+  const data = await fetch(`https://api.mercadolibre.com/items/${id}`);
   const items = await data.json();
-	JSON.parse(data).forEach((product) =>{
-		items.push(product)
-	})
-  return {items}
+
+  res.json({items})
 })
 
 var server  = app.listen(3001, ()=> 
